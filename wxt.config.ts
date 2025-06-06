63bd31b7e509650d35d5e67e1095e524fd95a6b2
Maksim Yin <yinmaksim@gmail.com>
2025-06-06 13:05:26 -0700

    Initialized boilerplate WXT extension (still has basic vite couter application)

---
+++ wxt.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'wxt';
+
+// See https://wxt.dev/api/config.html
+export default defineConfig({
+  modules: ['@wxt-dev/module-react'],
+  srcDir: 'src',
+  manifest: {
+    permissions: ['tabs', 'scripting'],
+    host_permissions: ['<all_urls>'],
+}
+});
